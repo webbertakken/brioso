86148ef9cc216b50142c7b9c4c88cafaf4cdd735
Webber <webber@takken.io>
2023-04-20 23:09:25 +0200

    initial commit

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,9 @@
+// Imports
+import { describe, it, expect } from 'vitest'
+
+// Tests
+describe('Renders main page correctly', async () => {
+  it('Should render the page correctly', async () => {
+    expect(true).toBeTruthy()
+  })
+})
