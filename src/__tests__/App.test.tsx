0793f5c59d5c600a4b30c8cf97f314e1255b3a3a
Webber <webber@takken.io>
2023-04-21 00:04:14 +0200

    chore: test setup

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,41 @@
-// Imports
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, afterEach } from 'vitest'
+import { render, screen, cleanup } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import App from '../App'
 
-// Tests
-describe('Renders main page correctly', async () => {
-  it('Should render the page correctly', async () => {
-    expect(true).toBeTruthy()
+describe('App', async () => {
+  afterEach(() => cleanup())
+
+  it('renders correctly', async () => {
+    // Arrange
+    await render(<App />)
+    const h1 = await screen.queryByText('Vite + React')
+
+    // Assert
+    expect(h1).toBeInTheDocument()
+  })
+
+  it('shows the button count set to 0', async () => {
+    // Arrange
+    await render(<App />)
+    const button = await screen.queryByText('count is 0')
+
+    // Assert
+    expect(button).toBeInTheDocument()
+  })
+
+  it('updates the button text after clicking it', async () => {
+    // Arrange
+    const user = userEvent.setup()
+    await render(<App />)
+    const button = await screen.queryByText('count is 0')
+
+    // Act
+    await user.click(button as HTMLElement)
+    await user.click(button as HTMLElement)
+    await user.click(button as HTMLElement)
+
+    // Assert
+    expect(button?.innerHTML).toStrictEqual('count is 3')
   })
 })
