942a80f0b8b8ac3d0df3a11c25a85032e5739aee
Webber Takken <webber@takken.io>
2023-04-24 23:27:13 +0200

    feat: add logo (#11)

--- src/panels/logo/Logo.tsx
+++ src/panels/logo/Logo.tsx
@@ -1,7 +1,15 @@
 import styles from './Logo.module.css'
+import { ReactComponent as LogoSvg } from '../../assets/logo.svg'
 
 const Logo = (): JSX.Element => {
-  return <div className={styles.logo}>Brioso</div>
+  return (
+    <div className={styles.logo}>
+      <div>
+        <LogoSvg height={54} />
+      </div>
+      <div>Brioso</div>
+    </div>
+  )
 }
 
 export default Logo
