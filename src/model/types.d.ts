7ab8c6c7ad2d33b5bbd869aa265107b4ba314b74
Webber Takken <webber@takken.io>
2023-04-29 18:07:16 +0200

    feat: delete song and files (#21)

--- src/model/types.d.ts
+++ src/model/types.d.ts
@@ -1,6 +1,7 @@
 interface SongData {
   id: string
   name: string
+  userId: string
 }
 
 interface PartData extends Upload {}
--- src/pages/practice/panels/songs/Song.tsx
+++ src/pages/practice/panels/songs/Song.tsx
@@ -1,6 +1,11 @@
 import styles from './Song.module.css'
 import { Link } from 'react-router-dom'
 import cx from 'classnames'
+import { useFirestore, useUser } from 'reactfire'
+import { Inventory } from '../../../../model/inventory.ts'
+import { doc, deleteDoc } from 'firebase/firestore'
+import { useNotify } from '../../../../core/notifications/hooks/useNotify.tsx'
+import { SyntheticEvent } from 'react'
 
 interface Props {
   title: string
@@ -9,10 +14,29 @@ interface Props {
 }
 
 const Song = ({ id, title, selected }: Props): JSX.Element => {
+  const { data: user } = useUser()
+  const firestore = useFirestore()
+  const songRef = doc(firestore, Inventory.getSongDocPath(user!.uid, id))
+  const notify = useNotify()
+
+  const deleteSong = async (e: SyntheticEvent) => {
+    e.preventDefault()
+    if (window.confirm(`This will permanently delete ${title} and all its parts. Are you sure?`)) {
+      await notify.promise(deleteDoc(songRef), {
+        loading: `deleting ${title}...`,
+        error: (error) => `${error}`,
+        success: `${title} was successfully deleted`,
+      })
+    }
+  }
+
   return (
     <Link to={`/practice/${id}`} className={cx(styles.song, { [styles.selected]: selected })}>
       <div className={styles.icon}>🎵</div>
       <div className={styles.title}>{title}</div>
+      <div className={cx(styles.icon, styles.delete)} onClick={deleteSong}>
+        ❌
+      </div>
     </Link>
   )
 }
