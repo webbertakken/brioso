89f4a0702230e4d2dd6e5978e8800bbff75ad94d
Webber <webber@takken.io>
2023-04-29 02:12:06 +0200

    feat: selecting songs

--- src/pages/practice/panels/songs/Song.tsx
+++ src/pages/practice/panels/songs/Song.tsx
@@ -1,14 +1,16 @@
 import styles from './Song.module.css'
 import { Link } from 'react-router-dom'
+import cx from 'classnames'
 
 interface Props {
   title: string
   id: string
+  selected: boolean
 }
 
-const Song = ({ id, title }: Props): JSX.Element => {
+const Song = ({ id, title, selected }: Props): JSX.Element => {
   return (
-    <Link to={`/practice/${id}`} className={styles.song}>
+    <Link to={`/practice/${id}`} className={cx(styles.song, { [styles.selected]: selected })}>
       <div className={styles.icon}>🎵</div>
       <div className={styles.title}>{title}</div>
     </Link>
