d53a0171adf5c5914f7a9c24ceaa2f5c17108bbe
Webber Takken <webber@takken.io>
2023-06-03 01:14:08 +0200

    feat: part controls (#30)
    * feat: improve error message
    
    * feat: part controls

--- src/pages/practice/panels/parts/PartsPanel.tsx
+++ src/pages/practice/panels/parts/PartsPanel.tsx
@@ -7,9 +7,10 @@ import ErrorBoundary from '../../../../core/react/display/ErrorBoundary.tsx'
 import { useFirestore, useFirestoreCollectionData, useUser } from 'reactfire'
 import { useParams } from 'react-router-dom'
 import Parts from './Parts.tsx'
-import { collection } from 'firebase/firestore'
+import { collection, CollectionReference } from 'firebase/firestore'
 import { Inventory } from '../../../../model/inventory.ts'
-import { CollectionReference } from 'firebase/firestore'
+import { useRef } from 'react'
+import { SharedControls } from './SharedControls.tsx'
 
 interface Props {}
 
@@ -18,15 +19,29 @@ const PartsPanel = ({}: Props): JSX.Element => {
   const { songId = 'No song selected', partId = '' } = useParams()
 
   const firestore = useFirestore()
-  const partsRef = collection(
+  const partsCollectionRef = collection(
     firestore,
     Inventory.getPartsPath(user!.uid, songId),
   ) as CollectionReference<PartData>
-  const { data: parts } = useFirestoreCollectionData(partsRef)
+  const { data: parts } = useFirestoreCollectionData(partsCollectionRef)
+
+  /**
+   * A map of part IDs to their audio elements.
+   * @see https://react.dev/learn/manipulating-the-dom-with-refs#how-to-manage-a-list-of-refs-using-a-ref-callback
+   */
+  const partsRef = useRef<Map<string, HTMLAudioElement> | null>(null)
+  const getRefMap = () => {
+    if (!partsRef.current) partsRef.current = new Map()
+
+    return partsRef.current
+  }
 
   return (
     <Panel title="Controls" icon={'🔸'}>
-      <PartControls />
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <PartControls getRefMap={getRefMap} songId={songId} partId={partId} parts={parts} />
+        <SharedControls getRefMap={getRefMap} songId={songId} partId={partId} parts={parts} />
+      </div>
 
       <PanelHeader title="Parts" icon={'🔸'} />
       <Parts>
@@ -38,12 +53,14 @@ const PartsPanel = ({}: Props): JSX.Element => {
           // Known parts after unidentified parts
           .sort(({ isKnownPart: a }, { isKnownPart: b }) => (a === b ? 0 : a ? 1 : -1))
           // Render
-          .map(({ id, name, metadata }) => (
+          .map(({ id, name, metadata, isKnownPart }) => (
             <Part
               key={id}
+              getRefMap={getRefMap}
               id={id}
               songId={songId}
               selected={id === partId}
+              muted={!isKnownPart}
               title={name}
               metadata={metadata}
             />
