06817c57202112c44f82c6b5b61d9f5c5e28482c
Webber <webber@takken.io>
2023-06-03 00:53:59 +0200

    feat: improve error message

--- src/core/react/display/ErrorBoundary.tsx
+++ src/core/react/display/ErrorBoundary.tsx
@@ -30,8 +30,27 @@ export default class ErrorBoundary extends React.Component<PropsWithChildren, St
       // You can render any custom fallback UI
       return (
         <div style={{ display: 'block' }}>
-          <span style={{ color: 'red' }}>Error</span>
-          {isDevelopment() && <span>: {this.state.error?.message}</span>}
+          <pre style={{ padding: '1rem', fontSize: '200%', margin: 0 }}>
+            <span style={{ color: 'red' }}>Error</span>
+            <span>: {this.state.error?.message}</span>
+          </pre>
+          {isDevelopment() && this.state.error && (
+            <>
+              <div style={{ padding: '1rem', fontSize: '150%', margin: 0 }}>
+                <pre>Method: {this.state.error.stack?.split('at')[1].trim().split(/\s/)[0]}</pre>
+              </div>
+
+              <div
+                style={{
+                  background: 'rgba(0,0,0,.15)',
+                  border: '2px dotted rgba(255,255,255,.15)',
+                  padding: '1rem',
+                }}
+              >
+                <pre>{this.state.error.stack}</pre>
+              </div>
+            </>
+          )}
         </div>
       )
     }
